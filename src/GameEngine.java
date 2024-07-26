23acabae4114d67ce80ad42db3357fdb62b75bf5
marafragoso <mfragososantos90@gmail.com>
2024-07-26 18:02:45 +0100

    initia project setup

---
+++ src/GameEngine.java
@@ -0,0 +1,13 @@
+package com.codeforall.online.damngame;
+
+public class GameEngine {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // Game game = new Game(50, 25, 100);
+
+        // game.init();
+        // game.start();
+        System.err.println("Hello World");
+    }
+}
