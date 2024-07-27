3ff2b623f7d7fcae3de4c05ed891ca3c57f4e50d
amaropmc <amaro.pmc.se@gmail.com>
2024-07-27 16:33:51 +0100

    Refactored Duck and Shark classes to extend from Animal super class

---
+++ src/animals/sharks/Shark.java
@@ -0,0 +1,12 @@
+package com.codeforall.online.damngame.animals.sharks;
+
+import com.codeforall.online.damngame.animals.Animal;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class Shark extends Animal {
+
+    public Shark(Picture picture) {
+        super(picture);
+    }
+}
+
