00331917f3577aa30ffadb37be7703ea9919aa31
marafragoso <mfragososantos90@gmail.com>
2024-07-28 03:21:36 +0100

    ducks

--- src/animals/ducks/Duck.java
+++ src/animals/ducks/Duck.java
@@ -1,11 +1,24 @@
-package com.codeforall.online.damngame.animals.ducks;
+package com.codeforall.online.damngame.animals.Ducks;
 
-import com.codeforall.online.damngame.animals.Animal;
+import com.codeforall.online.damngame.grid.Position;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
-public class Duck extends Animal {
+public class Duck {
+    protected Picture picture;
+    private Position position;
 
-    public Duck(Picture picture) {
-        super(picture);
+    public Duck(Picture picture, Position position) {
+        this.picture = picture;
+        this.position = position;
+        picture.draw();
+    }
+
+    public int getRightBorder(){
+        return this.picture.getMaxX();
+    }
+
+    public void moveRight(){
+        this.picture.translate(10,0);
+        this.position.updatePosition(10,0);
     }
 }
