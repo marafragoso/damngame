c85ba3f057f2def2cd2b528859807c038dc5dc7c
amaropmc <amaro.pmc.se@gmail.com>
2024-07-28 01:56:45 +0100

    Corrected duck and shark spawn rate and locations

--- src/animals/AnimalFactory.java
+++ src/animals/AnimalFactory.java
@@ -6,11 +6,13 @@ import com.codeforall.online.damngame.grid.Grid;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class AnimalFactory {
+    private static final String BIRD_IMAGE_ACCESS = "resources/Bird/";
+    private static final String SHARK_IMAGE_ACCESS = "resources/Shark/";
 
     public static Duck getNewDuck(Grid grid){
-        int randomY = (int)(Math.random() * (grid.rowToY(grid.getRows())) / 4) + Grid.PADDING;
+        int initialY = getInitialPosition(grid);
 
-        Picture duckPicture = new Picture(grid.columnToX(0), randomY, Grid.RESOURCE + "Bird/Bird2.png");
+        Picture duckPicture = new Picture(grid.columnToX(0), initialY, BIRD_IMAGE_ACCESS + "Bird1.png");
 
         return new Duck(duckPicture);
     }
@@ -18,8 +20,22 @@ public class AnimalFactory {
     public static Shark getNewShark(Grid grid){
         int randomX = (int)(Math.random() * (grid.columnToX(grid.getCols()) - 75) + Grid.PADDING);
 
-        Picture sharkPicture = new Picture(randomX, grid.rowToY(grid.getRows()) - 140, Grid.RESOURCE + "/Shark/shark1.png");
+        Picture sharkPicture = new Picture(randomX, grid.rowToY(grid.getRows()) - 140, SHARK_IMAGE_ACCESS + "shark1.png");
 
         return new Shark(sharkPicture);
+    }
+
+    private static int getInitialPosition(Grid grid) {
+        int randomY = (int)(Math.random() * (grid.rowToY(grid.getRows())) / 4) + Grid.PADDING;
+
+        if (randomY < 34) {
+            return 34;
+        } else if (randomY < 68) {
+            return 68;
+        } else if (randomY < 102) {
+            return 102;
+        } else if (randomY < 136) {
+            return 136;
+        } else return 170;
     }
 }
