966c23d77d0eda25df46d563426e63a90521ee0e
amaropmc <131875714+amaropmc@users.noreply.github.com>
2024-07-28 22:05:52 +0100

    Update Duck.java

--- src/animals/ducks/Duck.java
+++ src/animals/ducks/Duck.java
@@ -1,24 +1,11 @@
-package com.codeforall.online.damngame.animals.Ducks;
+package com.codeforall.online.damngame.animals.ducks;
 
-import com.codeforall.online.damngame.grid.Position;
+import com.codeforall.online.damngame.animals.Animal;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
-public class Duck {
-    protected Picture picture;
-    private Position position;
-
-    public Duck(Picture picture, Position position) {
-        this.picture = picture;
-        this.position = position;
-        picture.draw();
-    }
-
-    public int getRightBorder(){
-        return this.picture.getMaxX();
-    }
-
-    public void moveRight(){
-        this.picture.translate(10,0);
-        this.position.updatePosition(10,0);
+public class Duck extends Animal{
+    
+    public Duck(Picture picture) {
+        super(picture);
     }
 }
