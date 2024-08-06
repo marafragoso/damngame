6eaba3c63b00617b3f7014637323ec6b106b77a6
amaropmc <amaro.pmc.se@gmail.com>
2024-08-06 16:27:16 +0100

    Implemented the high score

--- src/HighScore/HighScore.java
+++ src/HighScore/HighScore.java
@@ -7,9 +7,13 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 import java.io.*;
 
+/**
+ * Reads and writes the highest score to a .txt file
+ */
 public class HighScore {
     private ScoreFile scoreFile;
-    private Picture highScoreSymbol;
+    private Picture newHighScoreSymbol;
+    private Picture highestScore;
     private int highScore;
 
     public HighScore() {
@@ -17,57 +21,45 @@ public class HighScore {
         this.highScore = this.scoreFile.readFile();
     }
 
-    public void draw(Grid grid) {
-        highScoreSymbol = new Picture(Grid.PADDING + grid.columnToX(grid.getCols()) - 140, Grid.PADDING, "resources/resources/highScore.png");
-        highScoreSymbol.draw();
+    // Displays the new high score on the canvas if the player achieves it
+    public void drawNewHighScore(Grid grid) {
+        newHighScoreSymbol = new Picture(Grid.PADDING + grid.columnToX(grid.getCols()) - 140, Grid.PADDING, "resources/resources/highScore.png");
+        newHighScoreSymbol.draw();
+    }
+
+    // Displays the highest score recorded on gameOver
+    public void drawHighestScore(Grid grid) {
+        if(newHighScoreSymbol != null) {
+            newHighScoreSymbol.delete();
+        }
+
+        this.highestScore = new Picture(Grid.PADDING + grid.columnToX(grid.getCols()) - 140, Grid.PADDING, "resources/resources/highestScore.png");
+        this.highestScore.draw();
     }
 
     public int getHighScore() {
         return this.highScore;
     }
 
+    public Picture getNewHighScoreSymbol() {
+        return this.newHighScoreSymbol;
+    }
+
+    public Picture getHighestScore() {
+        return this.highestScore;
+    }
+
+    // Register a new highscore and saves it to a .txt file
     public void setNewHighScore(int newHighScore) {
         this.highScore = newHighScore;
         this.scoreFile.writeFile(String.valueOf(newHighScore));
     }
 
-    public void displayHighScore() {
-        Text highScoreText = new Text(this.highScoreSymbol.getX() + 60,this.highScoreSymbol.getMaxY() + 20, String.valueOf(this.highScore));
+    // Displays the highest score
+    public void displayHighScore(Picture picture) {
+        Text highScoreText = new Text(picture.getX() + 60,picture.getMaxY() + 20, String.valueOf(this.highScore));
         highScoreText.setColor(Color.WHITE);
         highScoreText.grow(20,30);
         highScoreText.draw();
-    }
-
-    public class ScoreFile {
-        private final String fileName;
-
-        public ScoreFile(String fileName) {
-            this.fileName = fileName;
-        }
-
-        public void writeFile(String content) {
-            try {
-                FileWriter fileWriter = new FileWriter(this.fileName);
-                fileWriter.write(content);
-                fileWriter.close();
-            } catch (IOException e) {
-                System.err.println("Error writing to the file: " + e.getMessage());
-            }
-        }
-
-        public int readFile() {
-            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(this.fileName))) {
-                String line = bufferedReader.readLine();
-                int score = Integer.parseInt(line);
-
-                return score;
-
-            } catch (FileNotFoundException e) {
-                System.err.println("File not found: " + e.getMessage());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            return 0;
-        }
     }
 }
